e20e56544661bcee972f08b475c7c7a999728696
Nguyen Le Ngan Ha <nguyenlenganha923@gmail.com>
2025-10-02 10:54:31 +0700

    first commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "4.0.0-M2"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "org.example"
@@ -10,10 +12,45 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // Spring boot
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    // Spring dev tools
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // Thymeleaf
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+
+    // MSSQL JDBC driver
+    implementation("com.microsoft.sqlserver:mssql-jdbc:12.10.1.jre11")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+
+    // JBcrypt
+    implementation("org.mindrot:jbcrypt:0.4")
+
+    // Mail
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+
+    // Testing
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+// Enable annotation processing
+tasks.withType<JavaCompile> {
+    options.compilerArgs.add("-parameters")
+}
+
+tasks.bootRun {
+    sourceResources(sourceSets["main"])
 }
